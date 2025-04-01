2d651f4c7c1e326319cd4af45371d202fb42b843
Shrey Shakya <np03cs4a220474@heraldcollege.edu.np>
2025-04-02 02:50:40 +0545

    organized routings

--- legalaidfrontend/src/App.jsx
+++ legalaidfrontend/src/App.jsx
@@ -1,11 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage'; // Updated path for LandingPage
-import CaseTrackingPage from './pages/CaseTrackingPage';
 import LawyersPage from './pages/LawyersPage';
-import Dashboard from './pages/Dashboard'; // Import the Dashboard component
-import FAQ from './pages/FAQ';
-import AboutUs from './pages/AboutUs';
-import LawFirmProfile from './pages/LawFirmProfile';
 import LawyerDashboard from './pages/LawyerDashboard';
 import LawyerRegistration from './components/Auth/LawyerRegistration';
 import LawyerLogin from './components/Auth/LawyerLogin';
@@ -25,23 +20,8 @@ function App() {
                 {/* Landing Page */}
                 <Route path="/" element={<LandingPage />} />
 
-                {/* Case Tracking Page */}
-                <Route path="/case-tracking" element={<CaseTrackingPage />} />
-
                 {/* Lawyers Page */}
                 <Route path="/lawyers" element={<LawyersPage />} />
-
-                {/* Dashboard (Protected Route) */}
-                <Route path="/dashboard" element={<Dashboard />} />
-
-                {/* FAQ */}
-                <Route path="/faq" element={<FAQ />} /> 
-
-                {/* About Us */}
-                <Route path="/about" element={<AboutUs />} />
-
-                {/* Law Firm Profile */}
-                <Route path="/lawfirmprofile" element={<LawFirmProfile />} />
 
                 {/* Lawyer Dashboard */}
                 <Route path="/lawyerdashboard" element={<LawyerDashboard />} />
